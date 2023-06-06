537a838bea615d7765ab29ec762b099ef5683539
chrifrat1 <chrifrat1@gmail.com>
2023-06-06 13:31:21 +0200

    Arrays and map method

--- src/App.jsx
+++ src/App.jsx
@@ -7,43 +7,35 @@ import './App.css'
 
 class App extends Component {
   constructor(){  //run BEFORE the return of render
-     super(); 
-
-     this.state={   //used for referencing the state of the variable 'name' and access it in the js
-      name:{firstName:'Chris', lastName: 'Frattini'},
-      company: 'Frattini LTD'
-     }
+     super();
+     
+     
+    this.state={ //array declaration
+      monsters:[
+        {
+          name:'linda'
+        },
+        {
+          name: 'frank'
+        },
+        {
+          name: 'jackie'
+        },
+        {
+          name:'harry spotter'
+        }
+      ]
+    }
+    
   }
-
   render(){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite - React</h1>
-      <p>Hello {this.state.name.firstName} {this.state.name.lastName}, 
-      I work at {this.state.company}</p>  
-      <button 
-      onClick={()=>{
-        this.setState(()=>{
-          return{
-            name:{firstName:'Harry', lastName:'Spotter'} //returns the name variable.
-          };
-        }, ()=>{console.log(this.state)} // the callback function is run only after the state change happened
-        );  
-      }}>
-         Change Name
-         </button>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {this.state.monsters.map((monster)=>{ //the map method allows to access the array. 
+      //create a temporary object (monster) and use it iterate through the array and generate an h1 on the wp
+        return <h1>{monster.name}</h1>
+      })}
+    </div>
   );
   }
 }
