03162b2bb07583cd59ca0fd74b1618193dcfd190
chrifrat1 <chrifrat1@gmail.com>
2023-06-06 13:43:49 +0200

    keys in mapping

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,20 @@ class App extends Component {
     this.state={ //array declaration
       monsters:[
         {
-          name:'linda'
+          name:'linda',
+          id:'dsjhbfjksd'
         },
         {
-          name: 'frank'
+          name: 'frank',
+          id:'kafjdf'
         },
         {
-          name: 'jackie'
+          name: 'jackie',
+          id:'fahhfaei'
         },
         {
-          name:'harry spotter'
+          name:'harry spotter',
+          id:'kdabkvcb'
         }
       ]
     }
@@ -33,7 +37,8 @@ class App extends Component {
     <div>
       {this.state.monsters.map((monster)=>{ //the map method allows to access the array. 
       //create a temporary object (monster) and use it iterate through the array and generate an h1 on the wp
-        return <h1>{monster.name}</h1>
+        return <div key={monster.id}><h1>{monster.name}</h1></div> //a key value is needed for efficiency of the page
+                                                        //react will only update the needed component instead of the entire page
       })}
     </div>
   );
