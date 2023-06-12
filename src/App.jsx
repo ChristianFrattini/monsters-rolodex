6c3dd8986a959f19675bc790236c83117da4480c
chrifrat1 <chrifrat1@gmail.com>
2023-06-12 18:09:39 +0200

    storing original data

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ class App extends Component {
      
      
     this.state={ //array declaration
-      monsters:[]  //initialiase as empty in order to take values from API
+      monsters:[],  //initialiase as empty in order to take values from API
+      searchField:'',  //intialise a search field variable (it is made accessible everywhere in the code)
       }
 
       console.log('constructor-runs first')
@@ -19,7 +20,7 @@ class App extends Component {
     
     componentDidMount(){    //method that is run only once and used to fetch data fro API
       console.log('componentDidMount-runs third')
-      fetch('https://jsonplaceholder.typicode.com/users')
+      fetch('https://jsonplaceholder.typicode.com/users') // fetches the content of an api
       .then((response)=>response.json())  //converts the content of the api into a json file
       .then((users)=>this.setState(()=>{
         return{monsters: users}} ,
@@ -32,19 +33,23 @@ class App extends Component {
   render(){
   console.log('render-runs second')
 
+  const filteredMonsters=this.state.monsters.filter((monster)=>{
+    return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+  }); // constant containing the filtered mosters. (they're gonna be saved into the monster array
+
   return (
     <div>
+
       <input className='search-box' type='search' placeholder='Search Monster'
       onChange={(event)=>{
-        const searchString=event.target.value.toLocaleLowerCase();
-        const filteredMonsters=this.state.monsters.filter((monster)=>{
-          return monster.name.toLocaleLowerCase().includes(searchString);
-        });
+        const searchField=event.target.value.toLocaleLowerCase(); //a constant is storing the name. lower cased
         this.setState(()=>{
-          return {monsters: filteredMonsters};
+          return {searchField}; //return the filtered name into the variable search field
         });
       }}></input>
-      {this.state.monsters.map((monster)=>{ //the map method allows to access the array. 
+
+
+      {filteredMonsters.map((monster)=>{ //the map method allows to access the array. 
       //create a temporary object (monster) and use it iterate through the array and generate an h1 on the wp
         return <div key={monster.id}><h1>{monster.name}</h1></div> //a key value is needed for efficiency of the page
                                                         //react will only update the needed component instead of the entire page
