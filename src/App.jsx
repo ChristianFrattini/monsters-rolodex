c803454d8d0a9ccfc0ef0c7c7a78e7b9f0d3ee32
chrifrat1 <chrifrat1@gmail.com>
2023-07-26 18:56:02 +0200

    Class to Functional Components + useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,48 @@
-import React, { useState } from 'react'
-import { Component } from 'react'
+//import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 
 import CardList from './components/card-list/card-list.component.jsx'
 import SearchBox from './components/search-box/search-box.component'
 import './App.css'
 
 
+const App = ()=>{
 
-class App extends Component {
-  constructor(){  //run BEFORE the return of render
-     super();
-     
-     
-    this.state={ //array declaration
-      monsters:[],  //initialiase as empty in order to take values from API
-      searchField:'',  //intialise a search field variable (it is made accessible everywhere in the code)
-      }
+  const [searchField,setSearchField] =useState(''); //usestate creates an array of 2 elememts [value,setvalue]
+  const [monsters,setMonsters]=useState([]);
+  const [filteredMonsters,setFilteredMonsters]=useState(monsters);
 
-      //console.log('constructor-runs first')
-    };
-    
-    componentDidMount(){    //method that is run only once and used to fetch data fro API
-      //console.log('componentDidMount-runs third')
-      fetch('https://jsonplaceholder.typicode.com/users') // fetches the content of an api
-      .then((response)=>response.json())  //converts the content of the api into a json file
-      .then((users)=>this.setState(()=>{
-        return{monsters: users}} ,
-        ()=>{console.log(this.state);}
-      ));   //monsters is set to be equal to users (variable)
-    }
+  console.log(searchField);
 
-    onSearchChange = (event)=>{ //external method
-      const searchField=event.target.value.toLocaleLowerCase(); //a constant is storing the name. lower cased
-      this.setState(()=>{
-        return {searchField}; //return the filtered name into the variable search field
-      });
-    }
+  useEffect(()=>{  //useEffect is used to prevent 'fetch' from infinitely being run.useEffect has callback function and array of dependency
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then((users)=>setMonsters(users))
+  },[]) //whenever the values in the array changes the callback function is called. 
+  //and since the function needs to be called only once, an empty array is passed.
 
-  
-  render(){
-  //console.log('render-runs second')
+  useEffect(()=>{ //this useEffect is used to only update the monsters and searchField values only when values change
+    const newFilteredMonsters=monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
-  const {monsters, searchField}= this.state; //allows to make the code more readeable by coder
-  const {onSearchChange}=this; //allows to make the code more readeable by coder
+    setFilteredMonsters(newFilteredMonsters);
 
-  const filteredMonsters=monsters.filter((monster)=>{
-    return monster.name.toLocaleLowerCase().includes(searchField);
-  }); // constant containing the filtered mosters. (they're gonna be saved into the monster array
+  },[monsters,searchField])
+
+  const onSearchChange= (event)=>{ //external method
+    const searchFieldString=event.target.value.toLocaleLowerCase(); //a constant is storing the name. lower cased
+    setSearchField(searchFieldString);
+  };
 
 
-  return (
-    <div>
+
+
+
+  return(
+    <div className='App'>
 
       <h1 className='app-title'>Monsters Rolodex</h1>
 
@@ -60,16 +50,14 @@ class App extends Component {
        className='monsters-search-box' />
 
 
-      {/*filteredMonsters.map((monster)=>{ //the map method allows to access the array. 
-      //create a temporary object (monster) and use it iterate through the array and generate an h1 on the wp
-        return <div key={monster.id}><h1>{monster.name}</h1></div> //a key value is needed for efficiency of the page
-                                                        //react will only update the needed component instead of the entire page
-      })
-      */}
+    
       <CardList monsters={filteredMonsters}/>
     </div>
   );
-  }
 }
+
+
+
+
 
 export default App
